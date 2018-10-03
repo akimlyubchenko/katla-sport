b10baffeabbb871c4e2e8d036e0924fb55560fbc
Akim Liubchenka <akim_liubchenka@epam.com>
2018-10-03 22:10:32 +0300

    Merge step1. Add Description property to ProductCategory model. Create a new migration AddCategoryDescription.

---
+++ KatlaSport.DataAccess/Migrations/201810031853203_AddCategoryDescription.cs
@@ -0,0 +1,22 @@
+namespace KatlaSport.DataAccess.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    /// <summary>
+    /// Adds Category Description
+    /// </summary>
+    /// <seealso cref="System.Data.Entity.Migrations.DbMigration" />
+    /// <seealso cref="System.Data.Entity.Migrations.Infrastructure.IMigrationMetadata" />
+    public partial class AddCategoryDescription : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.product_categories", "category_description", c => c.String(maxLength: 300));
+        }
+
+        public override void Down()
+        {
+            DropColumn("dbo.product_categories", "category_description");
+        }
+    }
+}
